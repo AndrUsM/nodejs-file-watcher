48f4abc785ca79bc5f029ff90f1704123ebc8e42
AndrUsM <oksud22@gmail.com>
2020-12-31 16:43:50 +0200

    [update] add application mode constants and path name of app mode file

--- src/cli/responders/watch/constants.js
+++ src/cli/responders/watch/constants.js
@@ -6,8 +6,25 @@ const previousFilesIdPath = path.resolve(baseDataPath, '.previousFilesID.dat');
 
 const applicationHistoryPath = path.resolve(baseDataPath, '.history.dat');
 
+const applicationModeConfigFilePath = path.join(baseDataPath, '.mode.dat');
+const applicationBrowserMode = 'browser';
+const applicationConsoleMode = 'browser';
+const applicationDefaultMode = 'both';
+const applicationModeList = [
+    applicationConsoleMode,
+    applicationDefaultMode,
+    applicationBrowserMode
+];
+
 module.exports = {
     currentFilesIdPath: currentFilesIdPath,
     previousFilesIdPath: previousFilesIdPath,
-    applicationHistoryPath: applicationHistoryPath
+    applicationHistoryPath: applicationHistoryPath,
+
+    applicationBrowserMode: applicationBrowserMode,
+    applicationConsoleMode: applicationConsoleMode,
+    applicationDefaultMode: applicationDefaultMode,
+    applicationModeList: applicationModeList,
+
+    applicationModeConfigFilePath: applicationModeConfigFilePath
 }
