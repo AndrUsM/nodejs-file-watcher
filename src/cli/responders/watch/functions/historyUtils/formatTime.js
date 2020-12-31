d29b95a53040bc1342340d40891ac26409cac704
AndrUsM <oksud22@gmail.com>
2020-12-31 16:48:32 +0200

    [create] format time to <YYYY.MM.DD HH:SS:MS>

---
+++ src/cli/responders/watch/functions/historyUtils/formatTime.js
@@ -0,0 +1,33 @@
+function formatTime(timeInMs) {
+    const dateTimeObject = new Date(timeInMs);
+
+    let date = fillEmptySpaces([
+        dateTimeObject.getDay(),
+        dateTimeObject.getMonth(),
+        dateTimeObject.getFullYear()
+    ]);
+
+    let time = fillEmptySpaces([
+        dateTimeObject.getHours(),
+        dateTimeObject.getMinutes(),
+        dateTimeObject.getSeconds(),
+        dateTimeObject.getMilliseconds()
+    ]);
+
+    return [
+        date.join('.'),
+        time.join(':')
+    ].join(' ');
+}
+
+function fillEmptySpaces(values) {
+    return values.map(item => {
+        return +item < 10 ? [
+            0,
+            item
+        ].join('') :
+            item;
+    })
+}
+
+module.exports = formatTime;
