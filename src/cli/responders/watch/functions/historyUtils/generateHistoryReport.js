45dce56413cf726ae2ef9457726f59bce2178673
AndrUsM <oksud22@gmail.com>
2020-12-31 16:48:59 +0200

    [refactoring] move large methods to folders

--- src/cli/responders/watch/functions/historyUtils/generateHistoryReport.js
+++ src/cli/responders/watch/functions/historyUtils/generateHistoryReport.js
@@ -1,7 +1,8 @@
 const fs = require('fs');
 const path = require('path');
+const { EVENT_REMOVE } = require('../Events/eventsList');
 const { getFileType } = require('../watchUtils/checkFileType');
-const generateFileId = require('../watchUtils/generateFileId');
+const formatTime = require('./formatTime');
 
 function generateHistoryReport(filePath, event, options) {
     let {
@@ -12,57 +13,78 @@ function generateHistoryReport(filePath, event, options) {
     deleted = typeof deleted === 'boolean' ? deleted : false;
 
     if (fs.existsSync(filePath)) {
-        const filename = path.basename(filePath);
-        const fileStats = fs.statSync(filePath);
-        const {
-            atime,
-            ctime,
-            mtime,
-            birthtime,
-            size,
-        } = fileStats;
-        return {
+        return _fileReport({
             event: event,
+            filePath: filePath
+        });
+    } else {
+        return _deletedFileReport({
+            savedFileData: savedFileData,
+            deleted: deleted
+        });
+    }
+}
+
+function _fileReport(parameters) {
+    const {
+        event,
+        filePath
+    } = parameters;
+
+    const filename = path.basename(filePath);
+    const fileStats = fs.statSync(filePath);
+    const {
+        atime,
+        ctime,
+        mtime,
+        birthtime,
+        size,
+    } = fileStats;
+
+    return {
+        event: event,
+        filename: filename,
+        size: size,
+        type: getFileType(fileStats),
+        path: filePath,
+        accessTime: formatTime(atime),
+        modificationTime: formatTime(mtime),
+        changeTime: formatTime(ctime),
+        creationTime: formatTime(birthtime)
+    }
+}
+
+function _deletedFileReport(parameters) {
+    const {
+        savedFileData,
+        deleted
+    } = parameters;
+
+    if (deleted) {
+        const {
+            filename,
+            path,
+            size,
+            type,
+            accessTime,
+            modificationTime,
+            changeTime,
+            creationTime,
+        } = savedFileData;
+
+        return {
+            event: EVENT_REMOVE,
             filename: filename,
             size: size,
-            type: getFileType(fileStats),
-            path: filePath,
-            fileId: generateFileId(filePath),
-            accessTime: +atime,
-            modificationTime: +mtime,
-            changeTime: +ctime,
-            creationTime: +birthtime
+            type: type,
+            path: path,
+            accessTime: accessTime,
+            modificationTime: modificationTime,
+            changeTime: changeTime,
+            creationTime: creationTime
         }
-    }
-    else {
-        if (deleted) {
-            const {
-                filename,
-                path,
-                size,
-                type,
-                fileId,
-                accessTime,
-                modificationTime,
-                changeTime,
-                creationTime,
-            } = savedFileData;
-
-            return {
-                event: 'removed',
-                filename: filename,
-                size: size,
-                type: type,
-                path: path,
-                fileId: fileId,
-                accessTime: accessTime,
-                modificationTime: modificationTime,
-                changeTime: changeTime,
-                creationTime: creationTime
-            }
-        } else {
-            console.log(`Cannot generate report. Path ${filePath} is unaccessable.`);
-        }
+    } else {
+        console.log(`Cannot generate report. Path ${filePath} is unaccessable.`);
     }
 }
 
