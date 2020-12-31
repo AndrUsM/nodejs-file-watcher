ed1a9e202f18b897d29bbeabf44da1f3fc0dc08a
AndrUsM <oksud22@gmail.com>
2020-12-31 16:50:13 +0200

    [create] check fs changes by updating files id in folders

--- src/cli/responders/watch/functions/historyUtils/historyChanged.js
+++ src/cli/responders/watch/functions/historyUtils/historyChanged.js
@@ -4,7 +4,10 @@ function historyChanged() {
     const currentFSState = readFilesId('current');
     const previousFSState = readFilesId('prev');
 
-    return JSON.stringify(currentFSState) === JSON.stringify(previousFSState);
+    return (
+        JSON.stringify(currentFSState) ===
+        JSON.stringify(previousFSState)
+    );
 }
 
 module.exports = historyChanged;
