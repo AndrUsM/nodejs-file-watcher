319b3494a94aefec2c2bd275d2ee29e74ec20c12
AndrUsM <oksud22@gmail.com>
2020-12-31 16:47:55 +0200

    [refactoring] move to this file filesystem events

---
+++ src/cli/responders/watch/functions/Events/eventsList.js
@@ -0,0 +1,13 @@
+const EVENT_CREATED = 'create';
+const EVENT_REMOVE = 'remove';
+const EVENT_EDITED = 'edit';
+const EVENT_ACCESS = 'access';
+const EVENT_UNHANDLED = 'lost';
+
+module.exports = {
+    EVENT_CREATED: EVENT_CREATED,
+    EVENT_REMOVE: EVENT_REMOVE,
+    EVENT_EDITED: EVENT_EDITED,
+    EVENT_ACCESS: EVENT_ACCESS,
+    EVENT_UNHANDLED: EVENT_UNHANDLED
+}
