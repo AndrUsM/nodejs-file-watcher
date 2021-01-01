fde10f6577873ad79e257f20f5245d558443659d
AndrUsM <oksud22@gmail.com>
2021-01-02 01:29:28 +0200

    [refactoring] move initialization function

--- src/cli/responders/watch/functions/watchUtils/watchFolderRecursively.js
+++ src/cli/responders/watch/functions/watchUtils/watchFolderRecursively.js
@@ -1,39 +1,50 @@
 const os = require('os');
 const fs = require('fs');
 const path = require('path');
-const { exec } = require('child_process'); 
+const { exec } = require('child_process');
 const { checkFsContent } = require('./checkFileType');
 const fsListener = require('./fsListener');
 const eventEmmiter = require('../Events/events');
 const saveToHistory = require('../historyUtils/saveToHistory');
 const consoleHistory = require('../dataUtils/consoleHistory');
 
-function watchFolderRecursively(parameters) {
+const initFunction = (parameters) => {
+    let {
+        callback,
+        initialization
+    } = parameters;
+
+    if (initialization) {
+        callback;
+        initialization = false;
+    }
+}
+
+function _watchFolderRecursively(parameters) {
     const { callback } = parameters;
+
     let {
         folderPath,
         initialization
     } = parameters;
 
+    initFunction({
+        callback: callback,
+        initialization: initialization
+    });
+
     let folders = [];
 
-    folderPath = path.join(os.homedir(), 'Pictures');
+    folderPath = path.resolve(os.homedir(), 'Pictures');
     const getFoldersProcess = exec(`find ${folderPath} -type d`);
 
     getFoldersProcess.stdout.on('data', chunk => {
         if (checkFsContent(folderPath, 'dir')) {
             folders.push(chunk)
         }
-    })
+    });
 
     fsListener(callback);
-
-    const initFunction = () => {
-        callback;
-        initialization = false;
-    }
-
-    if (initialization) initFunction();
 
     getFoldersProcess.on('exit', () => {
         // remove empty elements, home directory and present as array
@@ -68,6 +79,16 @@ function watchFolderRecursively(parameters) {
             });
         })
     })
+}
+
+function watchFolderRecursively(parameters) {
+    const {
+        folderPath
+    } = parameters;
+
+    fs.watch(folderPath, 'utf8', _ => {
+        _watchFolderRecursively(parameters);
+    });
 }
 
 module.exports = watchFolderRecursively;
