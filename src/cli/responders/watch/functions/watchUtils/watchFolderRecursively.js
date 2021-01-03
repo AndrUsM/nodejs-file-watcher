3cc4edcb91e8d249e1cfedad91c9ecca4867607e
AndrUsM <oksud22@gmail.com>
2021-01-03 23:53:35 +0200

    [update] perform logging data without copies

--- src/cli/responders/watch/functions/watchUtils/watchFolderRecursively.js
+++ src/cli/responders/watch/functions/watchUtils/watchFolderRecursively.js
@@ -7,8 +7,10 @@ const fsListener = require('./fsListener');
 const eventEmmiter = require('../Events/events');
 const saveToHistory = require('../historyUtils/saveToHistory');
 const consoleHistory = require('../dataUtils/consoleHistory');
+const { currentFilesIdPath } = require('../../constants');
 
 function updateFsData(parameters) {
+    const { callback } = parameters;
     let {
         folderPath,
     } = parameters;
@@ -41,7 +43,7 @@ function updateFsData(parameters) {
                     filePath: _path,
                     fileChangeEvent: fileChangeEvent
                 });
-                // callback;
+                callback;
             });
         })
     })
@@ -62,12 +64,11 @@ function _saveHistoryData(parameters) {
 }
 
 function watchFolderRecursively(parameters) {
-    const { callback } = parameters;
-
-    setInterval(() => {
-        callback;
-        updateFsData(parameters);
-    }, 1000);
+    fs.watchFile(currentFilesIdPath, { encoding: 'utf8' }, (current, prev) => {
+        const condition = current.size !== prev.size || current.atime !== current.atime;
+        if (condition)
+            updateFsData(parameters);
+    });
 }
 
 module.exports = watchFolderRecursively;
