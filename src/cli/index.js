5f5b247144ccb2cb0ec79b1d58735d27860b33fe
AndrUsM <oksud22@gmail.com>
2021-01-04 16:18:41 +0200

    [update] handle keyboard shortcuts, perfort saving commands to history

--- src/cli/index.js
+++ src/cli/index.js
@@ -1,6 +1,8 @@
 const Readline = require('readline');
 const commands = require('./commandList');
 const emmiter = require('./emmiters');
+const handleArrowKeys = require('./commandHistory/handleArrowKeys');
+const { saveToCommandsHistory } = require('./commandHistory/commandsHistory');
 
 const readline = Readline.createInterface({
     input: process.stdin,
@@ -15,12 +17,21 @@ cli.promptMessage = () => {
 }
 
 cli.initialize = () => {
+    handleArrowKeys();
     cli.promptMessage()
     readline.on('line', line => {
+        console.clear();
+        line = line.trim();
         const enteredCommand = line ? line.split(' ')[0] : '';
         let matcherCommand = Object.keys(commands).find(command => enteredCommand.startsWith(command));
 
-        if (matcherCommand) emmiter.emit(matcherCommand, line);
+        if (matcherCommand) {
+            emmiter.emit(matcherCommand, line);
+            saveToCommandsHistory({
+                command: matcherCommand,
+                line: line
+            });
+        }
         else console.info(`Command ${line} not found`);
 
         cli.promptMessage()
