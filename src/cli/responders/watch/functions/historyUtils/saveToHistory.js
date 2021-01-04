0f03008a35a935344c2251fdae2ced639c242856
AndrUsM <oksud22@gmail.com>
2021-01-04 16:22:58 +0200

    [update] simplify saving logic

--- src/cli/responders/watch/functions/historyUtils/saveToHistory.js
+++ src/cli/responders/watch/functions/historyUtils/saveToHistory.js
@@ -4,52 +4,30 @@ const {
     createFile
 } = require('../dataUtils/dataUtils');
 const { applicationHistoryPath } = require('../../constants');
-const generateHistoryReport = require('./generateHistoryReport');
 
 const specialSign = require('./constants');
-const distinctHistoryData = require('./distinctHistoryData');
 const dataObjectExpectedLength = 9;
 
-function saveToHistory(fileData) {
+function saveToHistory(parameters) {
     let {
-        filePath,
-        event,
-        savedFileData
-    } = fileData;
-
-    const isFileExist = fs.existsSync(filePath);
+        fileData
+    } = parameters;
 
     if (!fs.existsSync(applicationHistoryPath))
         createFile(applicationHistoryPath);
 
-    let data = generateHistoryReport(filePath, event, {
-        deleted: !isFileExist,
-        savedFileData: savedFileData
-    });
-    data = data && typeof data !== 'string' ? JSON.stringify(data) : data;
-
-    const dataLength = Object.values(JSON.parse(data)).length;
-    const checkDataLength = dataLength === dataObjectExpectedLength;
-
-    if (checkDataLength) {
-        const addSpecialSign = () => {
-            return [
-                data,
-                specialSign
-            ].join('');
-        }
-        try {
-            const appendAction = appendFile(applicationHistoryPath, addSpecialSign());
-            if (!savedFileData && Object.values(savedFileData).length) {
-                appendAction;
-            } else {
-                appendFile(applicationHistoryPath, addSpecialSign());
-            }
-        } catch (error) {
-            console.log(error);
-        }
+    const checkInsideFileData = Object.values(fileData).length === dataObjectExpectedLength
+    if (checkInsideFileData) {
+        appendFile(applicationHistoryPath, addSpecialSign(fileData));
     }
-    distinctHistoryData();
+}
+
+const addSpecialSign = (data) => {
+    data = typeof data !== 'string' ? JSON.stringify(data) : data;
+    return [
+        data,
+        specialSign
+    ].join('');
 }
 
 module.exports = saveToHistory;
