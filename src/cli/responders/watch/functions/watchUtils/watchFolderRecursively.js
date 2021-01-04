c41e3cf2b97bc03f5d5a513527faa604461a71f0
AndrUsM <oksud22@gmail.com>
2021-01-04 16:24:15 +0200

    [update] move saving to WatchFile.js, remove useless code

--- src/cli/responders/watch/functions/watchUtils/watchFolderRecursively.js
+++ src/cli/responders/watch/functions/watchUtils/watchFolderRecursively.js
@@ -3,10 +3,6 @@ const fs = require('fs');
 const path = require('path');
 const { exec } = require('child_process');
 const { checkFsContent } = require('./checkFileType');
-const fsListener = require('./fsListener');
-const eventEmmiter = require('../Events/events');
-const saveToHistory = require('../historyUtils/saveToHistory');
-const consoleHistory = require('../dataUtils/consoleHistory');
 const { currentFilesIdPath } = require('../../constants');
 
 function updateFsData(parameters) {
@@ -37,37 +33,20 @@ function updateFsData(parameters) {
         // onChange folder content
         folders.forEach(folder => {
             fs.watch(folder).on('change', (eventType, filename) => {
-                const _path = path.join(folder, filename);
-                const fileChangeEvent = eventEmmiter(_path);
-                _saveHistoryData({
-                    filePath: _path,
-                    fileChangeEvent: fileChangeEvent
-                });
                 callback;
             });
         })
     })
 }
 
-function _saveHistoryData(parameters) {
-    const {
-        filePath,
-        fileChangeEvent
-    } = parameters;
-
-    saveToHistory({
-        filePath: filePath,
-        event: fileChangeEvent,
-        savedFileData: {}
-    });
-    consoleHistory();
-}
-
 function watchFolderRecursively(parameters) {
-    fs.watchFile(currentFilesIdPath, { encoding: 'utf8' }, (current, prev) => {
+    const { callback } = parameters;
+    fs.watchFile(currentFilesIdPath, { encoding: 'utf8', persistent: true }, (current, prev) => {
         const condition = current.size !== prev.size || current.atime !== current.atime;
-        if (condition)
+        if (condition) {
             updateFsData(parameters);
+            callback;
+        }
     });
 }
 
