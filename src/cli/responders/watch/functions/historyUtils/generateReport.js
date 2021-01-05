bffbadfa9e67b0e147e48ea15e0027cbd852ce5f
AndrUsM <oksud22@gmail.com>
2021-01-05 14:58:58 +0200

    [create] generatate report function

---
+++ src/cli/responders/watch/functions/historyUtils/generateReport.js
@@ -0,0 +1,38 @@
+const path = require('path');
+const eventEmmiter = require('../events/events');
+const formatTime = require('./formatTime');
+const getFileType = require('../watchUtils/getFileType');
+const formatSize = require('../watchUtils/formatSize');
+
+function generateReport(items) {
+    const {
+        current,
+        previous,
+        filePath
+    } = items;
+    const {
+        atime,
+        ctime,
+        mtime,
+        birthtime,
+    } = current;
+
+    const filename = path.basename(filePath);
+    return {
+        event: eventEmmiter(filePath),
+        filename: filename,
+        size: [
+            `current: ${formatSize(current.size)}`,
+            `previous: ${formatSize(previous.size)}`,
+            `changes: ${formatSize(current.size - previous.size)}`
+        ].join('\n'),
+        type: getFileType(current),
+        path: filePath,
+        accessTime: formatTime(atime),
+        modificationTime: formatTime(mtime),
+        changeTime: formatTime(ctime),
+        creationTime: formatTime(birthtime)
+    }
+}
+
+module.exports = generateReport;
