2a3b4f144e6ff882cecc77dba94eb4b2539339e3
AndrUsM <oksud22@gmail.com>
2021-01-10 00:54:09 +0200

    [update] del key must clear line

--- src/cli/handleKeyboard/partials/del.js
+++ src/cli/handleKeyboard/partials/del.js
@@ -1,8 +1,28 @@
 const readline = require('readline');
-const { stdout } = require('process');
+const {
+    stdout,
+    stdin
+} = require('process');
+const {
+    getCurrentLine,
+    clearCurrentLine
+} = require('../currentLine');
+const { timeStamp } = require('console');
+const {
+    out,
+    messageType
+} = require('../../../lib/coloredOut/out');
 
+// BUG: stdin line not cleared properly, its will hide
 function deleteKeyAction() {
-    readline.clearLine(stdout, 1);
+    clearCurrentLine();
+    stdout.clearLine();
+    stdout.cursorTo(0);
+
+    out(
+        'Press <Enter> to continue...',
+        messageType.info
+    );
 }
 
 module.exports = deleteKeyAction;
